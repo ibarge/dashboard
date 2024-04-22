9d2b3dbe7caad21f99ee62244c5ba9d3392a606e
moeendehqan <moeen.dehqan@gmail.com>
2024-04-22 23:08:31 +0330

    table

--- src/sections/invoice/view/invoice-view.jsx
+++ src/sections/invoice/view/invoice-view.jsx
@@ -1,52 +1,112 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-import {TabulatorFull as Tabulator} from 'tabulator-tables';
+import { TabulatorFull as Tabulator } from 'tabulator-tables';
 
 import Card from '@mui/material/Card';
-import Stack from '@mui/material/Stack';
 
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 
 import Typography from '@mui/material/Typography';
 
-
 import Iconify from 'src/components/iconify';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 
 // ----------------------------------------------------------------------
 
 export default function InvoicePage() {
-
-
-
-
-
   const [dataVar, setDataVar] = useState([
-    {row:1, age:22, name:'moeen'},
-    {row:2, age:24, name:'farsh'},
-    {row:3, age:15, name:'pooya'},
-    {row:4, age:68, name:'ehsan'},
-    {row:5, age:42, name:'marzi'},
-  ])
+    { Title: 'باسکول ابوذر', taxid: 22, createat: '1402-10-12' , status:'موفق'},
+    { Title: 'پارکینگ شهرداری', taxid: 24, createat: '1402-10-01' , status:'پردازش'},
+    { Title: 'تجهیزات فروشگاه رفاه', taxid: 15, createat: '1402-09-18' , status:'موفق'},
+    { Title: 'حمل بار نخعی', taxid: 68, createat: '1402-11-22' , status:'موفق'},
+    { Title: 'جاده کمربندی', taxid: 68, createat: '1402-11-22' , status:'موفق'},
+    { Title: 'سرویس خودرو جهاد', taxid: 42, createat: '1403-01-18' , status:'خطا'},
+    { Title: 'باسکول ابوذر', taxid: 22, createat: '1402-10-12' , status:'موفق'},
+    { Title: 'پارکینگ شهرداری', taxid: 24, createat: '1402-10-01' , status:'پردازش'},
+    { Title: 'تجهیزات فروشگاه رفاه', taxid: 15, createat: '1402-09-18' , status:'موفق'},
+    { Title: 'حمل بار نخعی', taxid: 68, createat: '1402-11-22' , status:'موفق'},
+    { Title: 'سرویس خودرو جهاد', taxid: 42, createat: '1403-01-18' , status:'خطا'},
+    { Title: 'باسکول ابوذر', taxid: 22, createat: '1402-10-12' , status:'موفق'},
+    { Title: 'پارکینگ شهرداری', taxid: 24, createat: '1402-10-01' , status:'پردازش'},
+    { Title: 'تجهیزات فروشگاه رفاه', taxid: 15, createat: '1402-09-18' , status:'موفق'},
+    { Title: 'حمل بار نخعی', taxid: 68, createat: '1402-11-22' , status:'موفق'},
+    { Title: 'سرویس خودرو جهاد', taxid: 42, createat: '1403-01-18' , status:'خطا'},
+    { Title: 'باسکول ابوذر', taxid: 22, createat: '1402-10-12' , status:'موفق'},
+    { Title: 'پارکینگ شهرداری', taxid: 24, createat: '1402-10-01' , status:'پردازش'},
+    { Title: 'تجهیزات فروشگاه رفاه', taxid: 15, createat: '1402-09-18' , status:'موفق'},
+    { Title: 'حمل بار نخعی', taxid: 68, createat: '1402-11-22' , status:'موفق'},
+    { Title: 'سرویس خودرو جهاد', taxid: 42, createat: '1403-01-18' , status:'خطا'},
+    { Title: 'باسکول ابوذر', taxid: 22, createat: '1402-10-12' , status:'موفق'},
+    { Title: 'پارکینگ شهرداری', taxid: 24, createat: '1402-10-01' , status:'پردازش'},
+    { Title: 'تجهیزات فروشگاه رفاه', taxid: 15, createat: '1402-09-18' , status:'موفق'},
+    { Title: 'حمل بار نخعی', taxid: 68, createat: '1402-11-22' , status:'موفق'},
+    { Title: 'سرویس خودرو جهاد', taxid: 42, createat: '1403-01-18' , status:'خطا'},
+  ]);
 
-  const table = new Tabulator("#table", {
-    data:dataVar,
-    autoColumns:true,
-  });
+  useEffect(() => {
+    const table = new Tabulator('#table', {
+      data: dataVar,
+      layout:"fitColumns",
+      layoutColumnsOnNewData:true,
+      responsiveLayout:"hide",
+      textDirection:"rtl",
+      placeholder: 'داده ای برای نمایش موجود نیست',
+      placeholderHeaderFilter:"داده ای منطق نیست",
+      pagination:true,
+      paginationSize:5,
+      paginationSizeSelector:[5, 10, 20, 30, 50, 80, 130, true],
+      langs:{
+        "default":{
+            "pagination":{
+              "first":"نخست",
+              "first_title":"صفحه نخست",
+              "last":"آخرین",
+              "last_title":"اخرین صفحه",
+              "prev":"قبلی",
+              "prev_title":"صفحه قبل",
+              "next":"بعدی",
+              "next_title":"صفحه بعد",
+              "all":"همه",
+            },
+        }
+    },
+      columns: [
+        { title: 'عنوان', field: 'Title', widthGrow:4 ,responsive:0, headerFilter:"input", minWidth:120},
+        { title: 'شناسه یکتا', field: 'taxid', widthGrow:3 ,responsive:2, headerFilter:"input", minWidth:80},
+        { title: 'تاریخ', field: 'createat', widthGrow:2 ,responsive:3, headerFilter:"input", minWidth:60},
+        { title: 'وضعیت', field: 'status', widthGrow:2 ,responsive:1, headerFilter:"input", minWidth:40},
+      ],
+    });
+
+
+
+  }, [dataVar]);
 
   return (
     <Container>
-      <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-        <Typography variant="h4">Users</Typography>
-
+      <Grid2 container spacing={17}>
+        <Grid2 xs={4}>
+        <Typography variant="h4">صورت حساب</Typography>
+        </Grid2>
+        <Grid2 xs={8}>
+          {null}
+        </Grid2>
+        <Grid2 xs={5}>
+        <Container>
         <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
           افزودن
         </Button>
-      </Stack>
+        <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
+          دانلود
+        </Button>
 
+        </Container>
+        </Grid2>
+
+      </Grid2>
       <Card>
-
-        <Container id='table'/>
+        <Container id="table" />
       </Card>
     </Container>
   );
