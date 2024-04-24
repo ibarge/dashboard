e791dc35741b4ec3b2d5bb4fee506ac9396c8f3a
moeendehqan <moeen.dehqan@gmail.com>
2024-04-24 23:37:06 +0330

    logo

--- src/routes/sections.jsx
+++ src/routes/sections.jsx
@@ -4,10 +4,9 @@ import { Outlet, Navigate, useRoutes } from 'react-router-dom';
 import DashboardLayout from 'src/layouts/dashboard';
 
 export const IndexPage = lazy(() => import('src/pages/app'));
-export const BlogPage = lazy(() => import('src/pages/blog'));
+export const RegisterPage = lazy(() => import('src/pages/register'));
 export const InvoicePage = lazy(() => import('src/pages/invoice'));
 export const LoginPage = lazy(() => import('src/pages/login'));
-export const ProductsPage = lazy(() => import('src/pages/products'));
 export const Page404 = lazy(() => import('src/pages/page-not-found'));
 
 // ----------------------------------------------------------------------
@@ -25,13 +24,15 @@ export default function Router() {
       children: [
         { element: <IndexPage />, index: true },
         { path: 'invoice', element: <InvoicePage /> },
-        { path: 'products', element: <ProductsPage /> },
-        { path: 'blog', element: <BlogPage /> },
       ],
     },
     {
       path: 'login',
       element: <LoginPage />,
+    },
+    {
+      path: 'register',
+      element: <RegisterPage />,
     },
     {
       path: '404',
